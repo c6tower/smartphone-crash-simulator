948188291d97f0a75894d0dc151d953c44c97599
Minonono <clock2elizabeth@gmail.com>
2024-12-09 23:12:14 +0900

    feat(About): add about page

---
+++ src/components/pages/About.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>本サイトについて</h1>
+
+    <div>
+        <p>本サイトは、<a href="https://qiita.com/advent-calendar/2024/kuso-app">クソアプリ Advent Calendar 2024</a>向けに作られました。</p>
+        <p>真面目な用途は想定しておりませんので、ジョークとしてお楽しみください。</p>
+        <p>本サイトの利用によって何かしらの問題が発生した場合でも、運営側では対応できませんのでご了承願います。</p>
+        <p>また、効果音は、<a href="https://soundeffect-lab.info/">効果音ラボ</a>のフリー効果音素材を利用しています。</p>
+    </div>
+</template>
